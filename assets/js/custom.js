02ccfa50711bfe086df03c75ae6693911d2c0808
technomarkpratik <118794919+technomarkpratik@users.noreply.github.com>
2023-04-12 15:42:02 +0530

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+/* Scroll Down Start  */
+$(document).ready(function(){
+  $(".ct-btn-scroll").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+/* Scroll Down end */
